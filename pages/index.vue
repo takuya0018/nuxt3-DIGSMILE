70f11dadcdd2b9840600e85ee99e3db2cf57fb84
きくたたくや <gtk.9223@gmail.com>
2024-05-30 01:24:37 +0900

    初期設定

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+<template>
+  <div>
+    <h1>トップページ</h1>
+    <hr />
+    <NuxtLink to="/price">料金ページへ</NuxtLink>
+    <br />
+    <NuxtLink to="https://www.google.co.jp/">Google</NuxtLink>
+  </div>
+</template>
---
+++ pages/price.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+<template>
+  <div>
+    <h1>料金ページ</h1>
+    <hr />
+    <NuxtLink to="/">トップページへ</NuxtLink>
+  </div>
+</template>
